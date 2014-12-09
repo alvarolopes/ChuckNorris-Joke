5c3441d82fa2d2f78ff720e0ad7176d7666c9472
alvaro <alvaroalberto@gmail.com>
2014-12-09 00:31:55 -0200

    AngularJs Simple app created to a code interview
    A sample web application that automatically updates the web interface
    with a Chuck Norris Joke obtained through the following API:
    http://www.icndb.com/api/.

---
+++ scripts/app/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+ 
+    var app = angular.module('chuckNoris', []);
+
+    app.controller('ChuckNorrisCrtl', function ($interval, $http) {
+
+        var self = this;
+
+        this.getRandomJoke = function() 
+        {
+            $http.get('http://api.icndb.com/jokes/random').success(function(data) {
+                self.joke = data.value.joke;
+            });
+        };
+      
+        $interval(function () {
+            self.getRandomJoke();
+        }, 5000);
+
+        this.getRandomJoke();
+
+    });
+
+})();
